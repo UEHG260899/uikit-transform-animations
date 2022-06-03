17ddc0274607b1f4139478a005f8318ade23a891
Uriel Enrique <urielenrique99@gmail.com>
2022-06-02 21:49:25 -0500

    added fade animations to views

--- Flight Info/ViewController.swift
+++ Flight Info/ViewController.swift
@@ -33,100 +33,117 @@
 import UIKit
 
 class ViewController: UIViewController {
-  @IBOutlet var background: UIImageView!
-  
-  @IBOutlet var summaryIcon: UIImageView!
-  @IBOutlet var summary: UILabel!
-  
-  @IBOutlet var flightNumberLabel: UILabel!
-  @IBOutlet var gateNumberLabel: UILabel!
-  @IBOutlet var originLabel: UILabel!
-  @IBOutlet var destinationLabel: UILabel!
-  @IBOutlet var plane: UIImageView!
-  
-  @IBOutlet var statusLabel: UILabel!
-  @IBOutlet var statusBanner: UIImageView!
-
-  private let snowView = SnowView( frame: .init(x: -150, y:-100, width: 300, height: 50) )
+    @IBOutlet var background: UIImageView!
+    
+    @IBOutlet var summaryIcon: UIImageView!
+    @IBOutlet var summary: UILabel!
+    
+    @IBOutlet var flightNumberLabel: UILabel!
+    @IBOutlet var gateNumberLabel: UILabel!
+    @IBOutlet var originLabel: UILabel!
+    @IBOutlet var destinationLabel: UILabel!
+    @IBOutlet var plane: UIImageView!
+    
+    @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var statusBanner: UIImageView!
+    
+    private let snowView = SnowView( frame: .init(x: -150, y:-100, width: 300, height: 50) )
 }
 
 //MARK:- UIViewController
 extension ViewController {
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    // Add the snow effect layer
-    let snowClipView = UIView( frame: view.frame.offsetBy(dx: 0, dy: 50) )
-    snowClipView.clipsToBounds = true
-    snowClipView.addSubview(snowView)
-    view.addSubview(snowClipView)
-
-    // Start rotating the flights
-    changeFlight(to: .londonToParis, animated: false)
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Add the snow effect layer
+        let snowClipView = UIView( frame: view.frame.offsetBy(dx: 0, dy: 50) )
+        snowClipView.clipsToBounds = true
+        snowClipView.addSubview(snowView)
+        view.addSubview(snowClipView)
+        
+        // Start rotating the flights
+        changeFlight(to: .londonToParis, animated: false)
+    }
 }
 
 private extension ViewController {
-  //MARK:- Animations
+    //MARK:- Animations
+    
+    func fade(to image: UIImage, showEffects: Bool) {
+        let tempView = UIImageView(frame: background.frame)
+        tempView.image = image
+        tempView.alpha = 0
+        tempView.center.y += 20
+        tempView.bounds.size.width = background.bounds.width * 1.3
+        background.superview!.insertSubview(tempView, aboveSubview: background)
+        
+        UIView.animate(withDuration: 0.5) {
+            tempView.alpha = 1
+            tempView.center.y -= 20
+            tempView.bounds.size.width = self.background.bounds.width
+        } completion: { _ in
+            self.background.image = image
+            tempView.removeFromSuperview()
+        }
+        
+        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseOut) {
+            self.snowView.alpha = showEffects ? 1 : 0
+        }
 
-  func fade(to image: UIImage, showEffects: Bool) {
-		//TODO: Create a crossfade animation for the background
-
-    //TODO: Create a fade animation for snowView
-  }
-  
-  func move(label: UILabel, text: String, offset: CGPoint) {
-    //TODO: Animate a label's translation property
-  }
-  
-  func cubeTransition(label: UILabel, text: String) {
-		//TODO: Create a faux rotating cube animation
-  }
-  
-  func depart() {
-		//TODO: Animate the plane taking off and landing
-  }
-  
-  func changeSummary(to summaryText: String) {
-		//TODO: Animate the summary text
-  }
-
-  func changeFlight(to flight: Flight, animated: Bool = false) {
-		// populate the UI with the next flight's data
-    background.image = UIImage(named: flight.weatherImageName)
-    originLabel.text = flight.origin
-    destinationLabel.text = flight.destination
-    flightNumberLabel.text = flight.number
-    gateNumberLabel.text = flight.gateNumber
-    statusLabel.text = flight.status
-    summary.text = flight.summary
-
-    if animated {
-      // TODO: Call your animation
-    } else {
 
     }
     
-    // schedule next flight
-    delay(seconds: 3) {
-      self.changeFlight(
-        to: flight.isTakingOff ? .parisToRome : .londonToParis,
-        animated: true
-      )
+    func move(label: UILabel, text: String, offset: CGPoint) {
+        //TODO: Animate a label's translation property
     }
-  }
-  
-  //MARK:- utility methods
-  func duplicate(_ label: UILabel) -> UILabel {
-    let newLabel = UILabel(frame: label.frame)
-    newLabel.font = label.font
-    newLabel.textAlignment = label.textAlignment
-    newLabel.textColor = label.textColor
-    newLabel.backgroundColor = label.backgroundColor
-    return newLabel
-  }
+    
+    func cubeTransition(label: UILabel, text: String) {
+        //TODO: Create a faux rotating cube animation
+    }
+    
+    func depart() {
+        //TODO: Animate the plane taking off and landing
+    }
+    
+    func changeSummary(to summaryText: String) {
+        //TODO: Animate the summary text
+    }
+    
+    func changeFlight(to flight: Flight, animated: Bool = false) {
+        // populate the UI with the next flight's data
+        originLabel.text = flight.origin
+        destinationLabel.text = flight.destination
+        flightNumberLabel.text = flight.number
+        gateNumberLabel.text = flight.gateNumber
+        statusLabel.text = flight.status
+        summary.text = flight.summary
+        
+        if animated {
+            fade(to: UIImage(named: flight.weatherImageName)!, showEffects: flight.showWeatherEffects)
+        } else {
+            background.image = UIImage(named: flight.weatherImageName)
+        }
+        
+        // schedule next flight
+        delay(seconds: 3) {
+            self.changeFlight(
+                to: flight.isTakingOff ? .parisToRome : .londonToParis,
+                animated: true
+            )
+        }
+    }
+    
+    //MARK:- utility methods
+    func duplicate(_ label: UILabel) -> UILabel {
+        let newLabel = UILabel(frame: label.frame)
+        newLabel.font = label.font
+        newLabel.textAlignment = label.textAlignment
+        newLabel.textColor = label.textColor
+        newLabel.backgroundColor = label.backgroundColor
+        return newLabel
+    }
 }
 
 private func delay(seconds: TimeInterval, execute: @escaping () -> Void) {
-  DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: execute)
+    DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: execute)
 }
